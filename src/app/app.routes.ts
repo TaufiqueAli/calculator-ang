db3471527d1678823329d7b7815e5b30b3596326
TaufiqueAli <taufique.ali@itdoseinfo.com>
2025-04-11 13:24:20 +0530

    code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CalculatorComponent } from './component/calculator/calculator.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'calculator', component:CalculatorComponent},
+    {path:'**', component: PageNotFoundComponent }
+];
---
+++ src/app/component/calculator/calculator.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { evaluate } from 'mathjs';
+
+
+@Component({
+  selector: 'app-calculator',
+  standalone: true, // ✅ this is the missing piece
+  imports: [FormsModule],
+  templateUrl: './calculator.component.html',
+  styleUrl: './calculator.component.css'
+})
+export class CalculatorComponent {
+  enterValue: string = '';
+  finalResult: string = '';
+
+  onInputChange(value: string) {
+    const opentCount = (this.enterValue.match(/\(/g) || [])?.length;
+    const closeCount = (this.enterValue.match(/\)/g) || [])?.length;
+
+    if (value === '=') {
+      this.evaluateExpression();
+    } else if (value === 'C') {
+      this.enterValue = '';
+    } else if (value === 'Back') {
+
+      this.enterValue = this.enterValue.slice(0, -1);
+      const lastChar = this.enterValue[this.enterValue.length - 1];
+      if (!isNaN(Number(lastChar))) {
+        this.evaluateExpression();
+      }
+
+    } else if(value === '('){
+      opentCount > closeCount ? this.enterValue += ')' : this.enterValue += '(';
+      opentCount > closeCount && this.evaluateExpression();
+    }else {
+      this.enterValue += value;
+      if(!('% x + - ÷ ('?.includes(value)) && opentCount===closeCount){
+        this.evaluateExpression()
+      }
+    }
+    
+  }
+
+  clearInput():void{
+    this.enterValue=''
+    this.finalResult=''
+  }
+
+  evaluateExpression() {
+    try {
+      const expression = this.enterValue
+        .replace(/x/g, '*')
+        .replace(/÷/g, '/')
+        .replace(/(\d+)%(\d+)/g, '($1/100)*$2')
+        .replace(/(\d+)%/g, '($1/100)')       
+        .replace(/(\d)(\()/g, '$1*(');         
+  
+      const result = evaluate(expression);
+  
+      // Round and clean trailing zeros
+      this.finalResult = parseFloat(result)
+        .toFixed(4)
+        .replace(/\.?0+$/, '')
+        .toString();
+    } catch (error) {
+      this.finalResult = 'Error';
+    }
+  }
+  
+
+  calculateResult():void{
+    this.onInputChange("=")
+  }
+}
